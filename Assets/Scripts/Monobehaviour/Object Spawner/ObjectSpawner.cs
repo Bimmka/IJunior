e0c0f2472f7662b1d7898dda05bf55c7cd691e10
Bimmka <bibimmka@gmail.com>
2021-02-28 18:12:35 +0400

    Рандомный спавнер

---
+++ Assets/Scripts/Monobehaviour/Object Spawner/ObjectSpawner.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour, IObjectSpawner
+{
+    public void SpawnObject(GameObject objectForSpawn)
+    {
+        Instantiate(objectForSpawn, transform.position, objectForSpawn.transform.rotation);
+    }
+}
+
+public interface IObjectSpawner
+{
+    void SpawnObject(GameObject objectForSpawn);
+}
